331b7ae84359d70df4240234649fc15dc2f7cb25
piolad <piolad2003@gmail.com>
2024-02-24 14:21:13 +0100

    update background, fix css missing semicolon, additional small fixes and changes

--- script.js
+++ script.js
@@ -1,70 +1,86 @@
-text_whoIam = null
-phrasesWhoIam = ["programmer", "tech enthusiast", "game creator", "machine learning enthusiast", "Computer Science student"]
+text_whoIam = null;
+phrasesWhoIam = [
+  "programmer",
+  "tech enthusiast",
+  "DevOps engineer",
+  "Computer Science student",
+];
 
 //string.includes polyfill - for older browser users
 if (!String.prototype.includes) {
-    String.prototype.includes = function(search, start) {
-      'use strict';
-      if (typeof start !== 'number') {
-        start = 0;
-      }
-  
-      if (start + search.length > this.length) {
-        return false;
-      } else {
-        return this.indexOf(search, start) !== -1;
-      }
-    };
-  }
-
-  delay = 200
-  
-  var i = 1;
-  function whoiamLoop(){
-
-      if(!done){
-        changeWhoamiText(text_whoIam, phrasesWhoIam[i], delay)
-        i = (i+1)%phrasesWhoIam.length;
-        console.log("I: " + i)
-      }
-      setTimeout(whoiamLoop, 7000)
+  String.prototype.includes = function (search, start) {
+    "use strict";
+    if (typeof start !== "number") {
+      start = 0;
     }
-    
-window.onload = () => {
-    text_whoIam = document.getElementById("headline").getElementsByTagName("span")[0]     
 
-
-
-    setTimeout(whoiamLoop, 3000)
+    if (start + search.length > this.length) {
+      return false;
+    } else {
+      return this.indexOf(search, start) !== -1;
+    }
+  };
 }
 
-done = false
-function changeWhoamiText(txtElem, newTxt, delay){
+delay = 200;
 
-    if(done) return
-    done = true
+var i = 1;
+function whoiamLoop() {
+  if (!done) {
+    changeWhoamiText(text_whoIam, phrasesWhoIam[i], delay);
+    i = (i + 1) % phrasesWhoIam.length;
+    console.log("I: " + i);
+  }
+  setTimeout(whoiamLoop, 7000);
+}
 
-    function removal_loop(){
-        if(txtElem.innerHTML.length > 0){
-            txtElem.innerHTML = txtElem.innerHTML.substring(0, txtElem.innerHTML.length-1)
-            setTimeout(removal_loop, delay)
+window.onload = () => {
+  document
+    .getElementById("faceimg-wrapper")
+    .addEventListener("mouseover", function () {
+      document.getElementById("faceimg-info").style.display = "block";
+      /* setTimeout(() => {
+    }, 600); */
+    });
+  document
+    .getElementById("faceimg-wrapper")
+    .addEventListener("mouseout", function () {
+      document.getElementById("faceimg-info").style.display = "none";
+    });
 
-        } else {
-            if(txtElem.innerHTML.length == 0)
-                adding_loop();
-        }
+  text_whoIam = document
+    .getElementById("headline")
+    .getElementsByTagName("span")[0];
+  setTimeout(whoiamLoop, 3000);
 
+  window.scrollTo(0,0)
+};
+
+done = false;
+function changeWhoamiText(txtElem, newTxt, delay) {
+  if (done) return;
+  done = true;
+
+  function removal_loop() {
+    if (txtElem.innerHTML.length > 0) {
+      txtElem.innerHTML = txtElem.innerHTML.substring(
+        0,
+        txtElem.innerHTML.length - 1
+      );
+      setTimeout(removal_loop, delay);
+    } else {
+      if (txtElem.innerHTML.length == 0) adding_loop();
     }
-    
-    function adding_loop(){
-        if(txtElem.innerHTML.length < newTxt.length){
-            txtElem.innerHTML += newTxt[txtElem.innerHTML.length]
-            setTimeout(adding_loop, delay)
-        } else {
-            done = false
-        }
+  }
 
+  function adding_loop() {
+    if (txtElem.innerHTML.length < newTxt.length) {
+      txtElem.innerHTML += newTxt[txtElem.innerHTML.length];
+      setTimeout(adding_loop, delay);
+    } else {
+      done = false;
     }
-    
-    removal_loop()
+  }
+
+  removal_loop();
 }
